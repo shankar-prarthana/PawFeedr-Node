d6625487a0e1c4eb28be419910557780fb533f67
shankar-prarthana <129578938+shankar-prarthana@users.noreply.github.com>
2023-07-02 03:07:20 +0530

    Update PetController.js

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -183,7 +183,7 @@ pythonScript.stdout.on('data', (data) => {
         const hour = parseInt(timeParts[0], 10);
         const minutes = parseInt(timeParts[1], 10);
         targetTime.setHours(hour, minutes);
-        console.log('currentTime: ' + JSON.stringify(newcurrentTimePet));
+        console.log('currentTime: ' + JSON.stringify(currentTime));
         console.log('targetTime: ' + JSON.stringify(targetTime));
 
         if (currentTime < targetTime) {
